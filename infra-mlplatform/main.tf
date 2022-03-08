313ac671db39def24267fc110e462038d35ccc74
shmbrg <sebastian.humberg@gmail.com>
2022-03-08 11:19:44 +0100

    remove state files

--- infra-mlplatform/main.tf
+++ infra-mlplatform/main.tf
@@ -5,29 +5,39 @@ resource "kubernetes_namespace" "mlplatform_namespace" {
     }
 }
 
+# Add random password for DB
+resource "random_password" "password"{
+    length = 24
+    special = true
+    override_special ="_%@"
+}
+
 # Deploy a postgres database with helm chart to cluster (database for model metadata)
 resource "helm_release" "postgresql" {
     name          = "postgresql"
+    namespace     = kubernetes_namespace.mlplatform_namespace.metadata[0].name
+
     repository    = "https://charts.bitnami.com/bitnami"
     chart         = "postgresql"
-    namespace     = kubernetes_namespace.mlplatform_namespace.metadata[0].name
 }
 
 # Deploy mlflow to cluster
 resource "helm_release" "mlflow" {
     name        = "mlflow"
+    namespace   = kubernetes_namespace.mlplatform_namespace.metadata[0].name
+
     repository  = "https://larribas.me/helm-charts"
     chart       = "mlflow"
     version     = "1.0.1"
-    namespace     = kubernetes_namespace.mlplatform_namespace.metadata[0].name
 
-    values      = [
+    values = [<<EOF
         backendStore:
-        postgres:
-            username: my_user
-            password: my_password
-            host: my_host
-            port: 5342
-            database: my_db
+            postgres:
+                username: postgresql
+                password: ${random_password.password.result}
+                host: my_host
+                port: 5342
+                database: postgresql
+        EOF
     ]
 }
